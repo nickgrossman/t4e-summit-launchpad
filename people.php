d13e8a967ffb5cc6c52506886fe67a95fc19f07f
Nick Grossman <nick@wrkng.net>
2012-06-01 17:14:53 -0700

    on-the-fly adjustments

---
+++ people.php
@@ -0,0 +1,9 @@
+ <?php 
+ $section = 'people';
+ include('includes/header.inc.php'); 
+ ?> 	 
+	
+	<iframe src="http://dotstorm.byconsens.us/d/tech4engage-people/" style="border:none;width:100%; height:2500px"></iframe>“”
+ 
+ <?php include('includes/footer.inc.php'); ?>
+ 
